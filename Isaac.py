2b8cac10ee46e68563cc49a28c635f6abdc7e0c4
dleh99 <83767383+dleh99@users.noreply.github.com>
2021-10-15 20:07:22 +0900

    객체들 클래스화

--- Isaac.py
+++ Isaac.py
@@ -21,15 +21,55 @@ def handle_events():
 open_canvas()
 #==============================================================
 os.chdir('d:/2DGP/Project/Sprite/Isaac')
-Isaac_Head = load_image('Isaac_Head.png')
-Isaac_Body = load_image('Isaac_Body.png')
+
+
+class Isaac_Body:
+    def __init__(self):
+        self.image = load_image('Isaac_Body.png')
+        self.frame = 0
+        self.x = 400
+        self.y = 300
+        self.direction = 0                      # 0 = 가만히, 1 = 왼쪽, 2 = 오른쪽, 3 = 위쪽, 4 = 밑쪽
+
+    def update(self):
+        if self.direction == 0:
+            self.frame = 0
+        else:
+            self.frame = (self.frame + 1) % 10
+
+    def draw(self):
+        self.image.clip_draw(self.frame * Body_Lenght, 0, Body_Lenght, Body_Raw, self.x, self.y)
+
+
+Issac_Body = Isaac_Body()
+
+
+class Isaac_Head:
+    def __init__(self):
+        self.image = load_image('Isaac_Head.png')
+        self.frame = 0
+        self.x = Issac_Body.x
+        self.y = Issac_Body.y + Body_Raw
+
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.clip_draw(0 * Head_Lenght, 0, Head_Lenght, Head_Raw, self.x, self.y)
+
+
+Issac_Head = Isaac_Head()
+
 #==============================================================
 while GamePlay:
+    Issac_Body.update()
+    Issac_Head.update()
+
     clear_canvas()
-    Isaac_Body.clip_draw(frame * Body_Lenght, 0, Body_Lenght, Body_Raw, 400, 300)
-    Isaac_Head.clip_draw(0 * Head_Lenght, 0, Head_Lenght, Head_Raw, 400, 300 + Body_Raw)
+    Issac_Body.draw()
+    Issac_Head.draw()
     update_canvas()
-    frame = (frame + 1) % 10
 
     handle_events()
     delay(0.05)
