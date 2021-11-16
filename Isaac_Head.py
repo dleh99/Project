4db319a3a453e75fafa5204e8be82e59a974ab55
dleh99 <83767383+dleh99@users.noreply.github.com>
2021-11-16 23:10:23 +0900

    테이블 만들었음, 근데 이거 어케 하냐

--- Isaac_Head.py
+++ Isaac_Head.py
@@ -11,4 +11,85 @@ TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 2
 
-W_DOWN, A_DOWN, S_DOWN, D_DOWN, W_UP, A_UP, S_UP, D_UP,
+W_DOWN, A_DOWN, S_DOWN, D_DOWN, W_UP, A_UP, S_UP, D_UP, LEFT_DOWN, RIGHT_DOWN, UP_DOWN, DOWN_DOWN = range(12)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_w): W_DOWN,
+    (SDL_KEYDOWN, SDLK_a): A_DOWN,
+    (SDL_KEYDOWN, SDLK_s): S_DOWN,
+    (SDL_KEYDOWN, SDLK_d): D_DOWN,
+    (SDL_KEYUP, SDLK_w): W_UP,
+    (SDL_KEYUP, SDLK_a): A_UP,
+    (SDL_KEYUP, SDLK_s): S_UP,
+    (SDL_KEYUP, SDLK_d): D_UP,
+    (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
+    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
+    (SDL_KEYDOWN, SDLK_UP): UP_DOWN,
+    (SDL_KEYDOWN, SDLK_DOWN): DOWN_DOWN,
+}
+
+class Go_Left:
+    pass
+
+class Go_Right:
+    pass
+
+class Go_Up:
+    pass
+
+class Go_Down:
+    pass
+
+next_state_table = {
+    Go_Left: {},
+    Go_Right: {},
+    Go_Up: {},
+    Go_Down: {}
+}
+
+class Isaac_head:
+
+    def __init__(self):
+        self.image = load_image('Isaac_Head.png')
+        self.font = load_font('ENCR10B.TTF', 16)
+        self.dir = 1                # 1 = 정면, 2 = 오른쪽, 3 = 위, 4 = 왼쪽
+        self.frame = 0
+        self.velocity = 0
+        self.x = 800 // 2
+        self.y = 600 // 2
+        self.size_x = 45
+        self.size_y = 42
+        self.event_que = []
+        self.cur_state = None
+        self.cur_state.enter(self, None)
+
+    def get_bb(self):
+        return self.x - 50, self.y - 50, self.x + 50, self.y + 50
+
+
+    def fire_ball(self):
+        pass
+
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+
+    def update(self):
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
+
+    def draw(self):
+        self.cur_state.draw(self)
+        self.font.draw(self.x - 60, self.y + 50, '(Time: %3.2f)' % get_time(), (255, 255, 0))
+        draw_rectangle(*self.get_bb())
+        #fill here
+
+
+    def handle_event(self, event):
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
